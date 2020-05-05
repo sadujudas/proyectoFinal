707e1de225dd0019f1f739f2b75b41b3675c8c85
JuanFernandez100 <gta.51510@gmail.com>
2020-05-05 14:20:11 -0500

    Update alumno.service.ts

--- src/app/services/alumno/alumno.service.ts
+++ src/app/services/alumno/alumno.service.ts
@@ -16,30 +16,30 @@ export class AlumnoService{
 
     ListarAlumnos(filtros:FiltroAlumno){
         let headers :  HttpHeaders = new HttpHeaders();
-        headers =  headers.append('Content-Type', 'application/json');
+        headers =  headers.append('Content-Type', 'application/json');//Agregamos los parametros para la authorizacion para que funcione la URL alumno
         headers =  headers.append('Authorization', 'srvSeguridad');
-        return this.http.get(`http://localhost:2526/api-proyecto/ListarAlumnos?Nom_Alumno=${filtros.nombre}&Ape_alumno=${filtros.apellido}&Dni=${filtros.dni}&Edad=${filtros.edad}&codigo=${filtros.codigo}`,{headers});
+        return this.http.get(`http://localhost:2526/api-proyecto/ListarAlumnos?Nom_Alumno=${filtros.nombre}&Ape_alumno=${filtros.apellido}&Dni=${filtros.dni}&Edad=${filtros.edad}&codigo=${filtros.codigo}`,{headers});//Agregamos la URL para listar un Alumno
     }
 
     AgregarAlumno(body:any){
         let headers :  HttpHeaders = new HttpHeaders();
-        headers =  headers.append('Content-Type', 'application/json');
+        headers =  headers.append('Content-Type', 'application/json');//Agregamos los parametros para la authorizacion para que funcione la URL alumno
         headers =  headers.append('Authorization', 'srvSeguridad');
-        return this.http.post(`http://localhost:2526/api-proyecto/AgregarAlumno`,body,{headers});
+        return this.http.post(`http://localhost:2526/api-proyecto/AgregarAlumno`,body,{headers});//Agregamos la URL para agregar un Alumno
     }
 
     EditarAlumno(body:any){
         let headers :  HttpHeaders = new HttpHeaders();
-        headers =  headers.append('Content-Type', 'application/json');
+        headers =  headers.append('Content-Type', 'application/json');//Agregamos los parametros para la authorizacion para que funcione la URL alumno
         headers =  headers.append('Authorization', 'srvSeguridad');
-        return this.http.put(`http://localhost:2526/api-proyecto/EditarAlumno`,body,{headers});
+        return this.http.put(`http://localhost:2526/api-proyecto/EditarAlumno`,body,{headers});//Agregamos la URL para editar un Alumno
     }
 
     EliminarAlumno(body:any){
         let headers :  HttpHeaders = new HttpHeaders();
-        headers =  headers.append('Content-Type', 'application/json');
+        headers =  headers.append('Content-Type', 'application/json');//Agregamos los parametros para la authorizacion para que funcione la URL alumno
         headers =  headers.append('Authorization', 'srvSeguridad');
-        return this.http.post(`http://localhost:2526/api-proyecto/EliminarAlumno?Alumnoid=${body}`,{headers});
+        return this.http.post(`http://localhost:2526/api-proyecto/EliminarAlumno?Alumnoid=${body}`,{headers});//Agregamos la URL para eliminar un Alumno
     }
 
 }
